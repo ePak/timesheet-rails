8514284542d12c12b7f2056804a2ca2a3dd8ba21
Eric Pak <erichyp@gmail.com>
2015-06-04 19:03:12 +1000

    react, webpack, hot-loading

---
+++ scripts/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Test.jsx</div>
+        <div>abc</div>
+      </div>
+    );
+  }
+}
---
+++ webpack.hot.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+  //devtool: 'eval',
+  // Set 'context' for Rails Asset Pipeline
+  //context: __dirname + '/app/assets/javascripts',
+  devServer: {
+    hot: true,
+    inline: true
+  },
+ 
+  entry: {
+    App: [
+      'webpack-dev-server/client?http://localhost:8080/javascripts/',
+      'webpack/hot/only-dev-server',
+      //'./app'
+      './scripts/app'
+    ]
+  },
+ 
+  output: {
+    filename: '[name]_wp_bundle.js', // Will output App_wp_bundle.js
+    path: __dirname + '/app/assets/javascripts', // Save to Rails Asset Pipeline
+    publicPath: 'http://localhost:8080/javascripts/' // Required for webpack-dev-server
+  },
+ 
+  // Require the webpack and react-hot-loader plugins
+  plugins: [  
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+ 
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+ 
+  module: {
+    // Load the react-hot-loader
+    loaders: [ { 
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'jsx-loader', 'babel?optional=runtime'],
+      include: path.join(__dirname, 'scripts')
+    } ]
+  }
+ 
+};
