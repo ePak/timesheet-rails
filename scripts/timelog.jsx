e7f23418a53540da8bac29c80a65eb52b31d7a8e
Eric Pak <erichyp@gmail.com>
2015-06-13 20:00:38 +1000

    add timelog.jsx

---
+++ scripts/timelog.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+const dateFormat = 'ddd, Do MMM YY';
+
+export default class TimeLog extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: true // TODO: make this dynamic
+    };
+  }
+
+  onDateChange(new_date) {
+    console.log(new_date.format(dateFormat));
+  }
+
+  render() {
+    let log = this.props;
+    let keyField, dateField, hoursField;
+    const date = log.date ? moment(log.date) : null;
+
+    if (this.state.isEditing) {
+      dateField = (
+        <DatePicker 
+          selected={ date }
+          onChange={ this.onDateChange.bind(this) } 
+          dateFormat={ dateFormat } />);
+      keyField = (<input value={ log.jiraKey } />);
+      hoursField = (<input value={ log.hours } />);
+    } else {
+      dateField = date ? date.format(dateFormat) : "";
+      keyField = log.jiraKey;
+      hoursField = log.hours;
+    }
+
+    return (
+      <div className="timelog">
+        <span className="timelog-date">{ dateField }</span>
+        <span className="timelog-key">{ keyField }</span>
+        <span className="timelog-hours">{ hoursField } hours</span>
+        <button className="timelog-save">Save</button>
+        <button className="timelog-cancel">Cancel</button>
+      </div>
+    );
+  }
+}
+
+TimeLog.propTypes = {
+  id: React.PropTypes.number.isRequired,
+  date: React.PropTypes.string.isRequired,
+  jiraKey: React.PropTypes.string.isRequired,
+  hours: React.PropTypes.number.isRequired
+};
+
--- scripts/timesheet.jsx
+++ scripts/timesheet.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import TimeLog from './timelog';
 
 export default class Timesheet extends React.Component {
   render() {
+    let log = {key:"DEMO-1", hours:2, date:"2016-06-13", id:-1};
     return (
       <div className="timesheet">
         <div className="header">Timesheet</div>
-        <div className="content">logs</div>
+        <div className="content">
+          <TimeLog
+            key= { log.id }
+            id={ log.id }
+            date={ log.date }
+            jiraKey={ log.key }
+            hours={ log.hours } />
+        </div>
       </div>
     );
   }
